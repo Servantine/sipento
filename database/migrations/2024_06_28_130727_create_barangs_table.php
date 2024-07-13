9d6f6585407ca85a0a9224224eed8f44ed8d7db5
unknown <servantin0123@gmail.com>
2024-07-13 13:59:42 +0700

    fix database barang

--- database/migrations/2024_06_28_130727_create_barangs_table.php
+++ database/migrations/2024_06_28_130727_create_barangs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('barang', function (Blueprint $table) {
+        Schema::create('barangs', function (Blueprint $table) {
             $table->id();
             $table->string('namabarang');
             $table->integer('stok');
